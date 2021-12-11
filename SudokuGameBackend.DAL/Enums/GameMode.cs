e0d79b2aaf2d8e48d89b982766028808e9f86885
Vlad Chemerskyi <chemerskyivlad@gmail.com>
2021-12-12 00:29:37 +0200

    Change GameMode enum

---
+++ SudokuGameBackend.DAL/Enums/GameMode.cs
@@ -0,0 +1,8 @@
+﻿
+public enum GameMode
+{
+    OnePuzzleEazy = 1,
+    OnePuzzleMedium = 2,
+    OnePuzzleHard = 3,
+    ThreePuzzles = 4
+}
---
+++ SudokuGameBackend.DAL/Interfaces/IDuelRatingRepository.cs
@@ -0,0 +1,17 @@
+﻿using SudokuGameBackend.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuGameBackend.DAL.Interfaces
+{
+    public interface IDuelRatingRepository
+    {
+        IEnumerable<DuelRating> GetAll();
+        DuelRating Get(string userId, GameMode gameMode);
+        IEnumerable<DuelRating> Find(Func<DuelRating, bool> predicate);
+        void Create(DuelRating duelRating);
+        void Update(DuelRating duelRating);
+        void Delete(string userId, GameMode gameMode);
+    }
+}
