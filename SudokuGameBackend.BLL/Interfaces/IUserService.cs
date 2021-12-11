74b436a2bff631b62b8e8850d7172b7ad6c14c14
Vlad Chemerskyi <chemerskyivlad@gmail.com>
2021-12-12 00:38:58 +0200

    Add UserService

---
+++ SudokuGameBackend.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using SudokuGameBackend.BLL.InputModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuGameBackend.BLL.Interfaces
+{
+    public interface IUserService
+    {
+        void AddUser(AddUserInput input);
+    }
+}
---
+++ SudokuGameBackend.BLL/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using SudokuGameBackend.BLL.InputModels;
+using SudokuGameBackend.BLL.Interfaces;
+using SudokuGameBackend.DAL.Entities;
+using SudokuGameBackend.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuGameBackend.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public UserService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        public void AddUser(AddUserInput input)
+        {
+            User user = mapper.Map<User>(input);
+            unitOfWork.UserRepository.Create(user);
+            unitOfWork.Save();
+        }
+    }
+}
