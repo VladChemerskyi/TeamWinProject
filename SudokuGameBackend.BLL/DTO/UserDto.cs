d2ce954d8c6fab242110139d0a17ff415012668c
Vlad Chemerskyi <chemerskyivlad@gmail.com>
2021-12-12 00:34:17 +0200

    Add UserDto and AddUserInput

---
+++ SudokuGameBackend.BLL/DTO/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuGameBackend.BLL.DTO
+{
+    public class UserDto
+    {
+        public string Id { get; set; }
+        public string CountryCode { get; set; }
+    }
+}
---
+++ SudokuGameBackend.BLL/InputModels/AddUserInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuGameBackend.BLL.InputModels
+{
+    public class AddUserInput
+    {
+        public string Id { get; set; }
+        public string CountryCode { get; set; }
+    }
+}
