01881ea66344d1325bc144220ca80afa36de8609
Vlad Chemerskyi <chemerskyivlad@gmail.com>
2021-12-12 02:03:24 +0200

    Add logging

--- SudokuGameBackend.BLL/Services/GameSessionService/GameSessionsService.cs
+++ SudokuGameBackend.BLL/Services/GameSessionService/GameSessionsService.cs
@@ -26,37 +26,19 @@ namespace SudokuGameBackend.BLL.Services
         // TODO: Make things async.
         public string CreateSession(GameMode gameMode, params string[] userIds)
         {
-            logger.LogDebug($"CreateSession. gameMode: {gameMode}, userIds: {string.Join(" ", userIds)}");
             var stopWatch = new Stopwatch();
             stopWatch.Start();
-            var session = new GameSession(gameMode, OnSessionAborted, OnSessionEnded, userIds);
+            var session = new GameSession(gameMode, userIds);
             stopWatch.Stop();
+            session.SetOnSessionEndAction(async () => await OnSessionEnd(session));
             sessions[session.Id] = session;
-            logger.LogDebug($"CreateSession success. sessionId: {session.Id}, gameMode: {gameMode}, " +
+            logger.LogDebug($"Session created. sessionId: {session.Id}, gameMode: {gameMode}, " + 
                 $"creationTime: {Math.Round(stopWatch.Elapsed.TotalSeconds, 2)}s, userIds: {string.Join(" ", userIds)}");
             return session.Id;
         }
 
-        private async Task OnSessionAborted(GameSession session)
+        private async Task OnSessionEnd(GameSession session)
         {
-            logger.LogDebug($"Session aborted. sessionId: {session.Id}");
-            using var scope = serviceScopeFactory.CreateScope();
-            var gameHubContext = scope.ServiceProvider.GetService<IHubContext<GameHub>>();
-            foreach (var userId in session.UserIds)
-            {
-                var userConnectionId = session.GetUserConnectionId(userId);
-                if (userConnectionId != null)
-                {
-                    await gameHubContext.Clients.Client(userConnectionId).SendAsync("GameAborted");
-                    logger.LogTrace($"GameAborted called. userId: {userId}, sessionId: {session.Id}");
-                }
-            }
-            this.DeleteSession(session.Id);
-        }
-
-        private async Task OnSessionEnded(GameSession session)
-        {
-            logger.LogDebug($"Session ended. sessionId: {session.Id}");
             if (session.UserIds.Count <= 2)
             {
                 using var scope = serviceScopeFactory.CreateScope();
